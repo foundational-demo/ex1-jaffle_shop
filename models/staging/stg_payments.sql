cff8c13a0dad469b1b3cc7d5cac29435705e0d84
orendar <Oren@foundational.io>
2024-03-13 14:43:19 +0200

    fix sql

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -5,35 +5,34 @@ with source as (
     our data in this project
     #}
     select
-        {{ ref('raw_payments') }}.id,
-        {{ ref('raw_payments') }}.order_id,
-        {{ ref('raw_payments') }}.payment_method,
-        {{ ref('raw_payments') }}.amount
-        {{ ref('raw_orders') }}.user_id as customer_id
-    from {{ ref('raw_payments') }}
-        join {{ ref('raw_orders') }}
-    on {{ ref('raw_payments') }}.order_id = {{ ref('raw_orders') }}.id
+        raw_payments.id,
+        raw_payments.order_id,
+        raw_payments.payment_method,
+        raw_payments.amount
+        raw_orders.user_id as customer_id
+    from {{ ref('raw_payments') }} as raw_payments
+        join {{ ref('raw_orders') }} as raw_orders
+    on raw_payments.order_id = raw_orders.id
     ),
 
     customer_address as (
-    select
-        id,
-        address,
-    from  {{ ref('raw_customers') }}
-    )
+        select
+            raw_customers.id,
+            raw_customers.address,
+        from  {{ ref('raw_customers') }} as raw_customers
+    ),
 
     renamed as (
-
-select
-    source.id as payment_id,
-    source.order_id,
-    source.payment_method,
-    -- `amount` is currently stored in cents, so we convert it to dollars
-    source.amount / 100 as amount
-    customer_address.address as billing_address
-from source
-    join customer_address
-on source.customer_id = customer_address.id
+        select
+            source.id as payment_id,
+            source.order_id,
+            source.payment_method,
+            -- `amount` is currently stored in cents, so we convert it to dollars
+            source.amount / 100 as amount
+            customer_address.address as billing_address
+        from source
+            join customer_address
+        on source.customer_id = customer_address.id
     )
 
 select * from renamed
